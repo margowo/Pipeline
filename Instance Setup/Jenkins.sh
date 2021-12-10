5a012a36b27565f682868a385595ba1e70214fde
mary <94859933+margowo@users.noreply.github.com>
2021-12-10 13:27:13 -0500

    Jenkins.sh user data updates

--- Instance Setup/Jenkins.sh
+++ Instance Setup/Jenkins.sh
@@ -7,8 +7,8 @@ systemctl enable apache2
 apt install awscli -y
 apt install openjdk-11-jdk -y
 apt install git
-git config --global user.name "Brian"
-git config --global user.email "brian.james.gomes@gmail.com"
+git config --global user.name "mary"
+git config --global user.email "m.margo@me.com"
 git config --global credential.helper store
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | tee \
         /usr/share/keyrings/jenkins-keyring.asc > /dev/null
@@ -18,7 +18,7 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
 apt-get update
 apt-get install jenkins -y
 apt install docker.io -y
-git clone https://github.com/bdgomey/Pipeline.git
+git clone https://github.com/margowo/Pipeline.git
 cd /opt
 wget https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz
 tar -xzvf apache-maven-3.8.4-bin.tar.gz
